8eede2f7036fa9085a195dbf2aba11e053f6348b
Wassim SAMAD <wass08@gmail.com>
2024-11-15 10:16:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
     <>
       <UI />
       <Loader />
-      <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
+      <Canvas shadows camera={{
+          position: [-0.5, 1, window.innerWidth > 800 ? 4 : 9],
+          fov: 45,
+        }}>
         <group position-y={0}>
           <Suspense fallback={null}>
             <Experience />
